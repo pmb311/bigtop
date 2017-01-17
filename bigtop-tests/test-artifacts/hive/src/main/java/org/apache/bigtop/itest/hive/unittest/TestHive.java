fb0afde3382d643f59947aa3637258175eca4670
Paul Back <pback@wayfair.com>
2017-01-17 15:34:09 -0500

    Add hive unit test module.

---
+++ bigtop-tests/test-artifacts/hive/src/main/java/org/apache/bigtop/itest/hive/unittest/TestHive.java
@@ -0,0 +1,12 @@
+package org.apache.bigtop.itest.hive.unittest;
+
+/**
+ * Created by pback on 1/17/17.
+ */
+public class TestHive {
+
+    public static void main(String[] args) {
+        String helloWorld = new String("Hello World");
+        System.out.println(helloWorld);
+    }
+}
