1b9d7eff1c4cad354d43f201a8c2c847d8589a4e
Paul Back <pback@wayfair.com>
2017-01-17 16:43:38 -0500

    Add basic hive test case.

--- bigtop-tests/test-artifacts/hive/src/main/java/org/apache/bigtop/itest/hive/unittest/TestHive.java
+++ bigtop-tests/test-artifacts/hive/src/main/java/org/apache/bigtop/itest/hive/unittest/TestHive.java
@@ -3,10 +3,39 @@ package org.apache.bigtop.itest.hive.unittest;
 /**
  * Created by pback on 1/17/17.
  */
+import org.apache.bigtop.itest.shell.Shell;
+import org.junit.AfterClass;
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+
 public class TestHive {
 
-    public static void main(String[] args) {
-        String helloWorld = new String("Hello World");
-        System.out.println(helloWorld);
+    private static Shell sh = new Shell("/bin/bash -s");
+
+    @AfterClass
+    public static void tearDown() {
+        // deletion of test table
+        sh.exec("hive -e \"DESCRIBE bigtop_test\"");
+        if (sh.getRet() == 0) {
+            sh.exec("hive -e \"DROP TABLE bigtop_test\"");
+            assertTrue("Drop table statement failed",
+                    sh.getRet() == 0);
+        }
+
+    }
+
+    @Test
+    public void testCreateTable() {
+
+        sh.exec("hive -e \"CREATE TABLE IF NOT EXISTS bigtop_test (id int, value string)\"");
+        assertTrue("Create table statement errored", sh.getRet() == 0);
+
+        sh.exec("hive -e \"DESCRIBE bigtop_test\"");
+        assertTrue("Describe table statement errored", sh.getRet() == 0);
+
+        sh.exec("hive -e \"SELECT * FROM bigtop_test\"");
+        assertTrue("Select statement errored", sh.getRet() == 0);
     }
 }
